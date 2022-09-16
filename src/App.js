4e931e4c32cbaf47d74861596980ec422de11ef2
abolfazl <a.khakzad1993@gmail.com>
2022-09-16 17:33:46 +0200

    renew

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
+import { taskContex } from "./taskContex";
 import TaskItems from "./TaskItems";
 import TopForm from "./TopForm";
 
-function App() {
+const App =()=> {
+
+  const[taskItems , setTaskItems] =useState([
+    {
+      id:1 ,
+      title : "first plan",
+      done: false
+    },
+    {
+      id:4 ,
+      title : "forth plan",
+      done: false
+    },
+    {
+      id:2 ,
+      title : "second plan" ,
+      done: true
+    },
+    {
+      id:3 ,
+      title : "third plan" ,
+      done: false
+    }
+  ])
   return (
-            <div className="container w-100 h-100">
+    <div className="container w-100 h-100">
                 <div className="row justify-content-center">
                   <div className="col-12 bg-light m-3 rounded ">
+              <taskContex.Provider  value={{
+                taskItems ,
+                setTaskItems
+              }}>
                     <TopForm/>
                     <TaskItems/>
+            </taskContex.Provider>
                   </div>
                 </div>
             </div>
--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-
+import { useContext } from 'react';
+import { taskContex } from './taskContex';
 
 const TaskItems = () => {
+    const {taskItems , setTaskItems}=useContext(taskContex); 
+    // const items=useContext(taskContex);  
     return (
         <>
             <ul className="list-group mt-3">
 
-<li className="list-group-item d-flex justify-content-between rounded">
+                {
+                    taskItems.map(t=>(
+                        <li 
+                       
+                        className={`list-group-item d-flex justify-content-between rounded mb-2 ${ t.done ? "list-group-item-success" : "" }`}> 
+                        {t.title}
+                        <span>
+                            {
+                                t.done ? (
+                                <i class="fa-solid fa-x text-warning me-3 pointer"></i>
+                                ) : (
+                                <i class="fa-solid fa-check text-success me-3 pointer"></i>
+                                )
+                            }
+                        
+                        
+                        <i class="fa-solid fa-trash text-danger pointer"></i>
+                        </span>
+                      </li>
+                    ))
+                }
+
+{/* <li className="list-group-item d-flex justify-content-between rounded">
   first plan
   <span>
   <i class="fa-solid fa-check text-success me-3 pointer"></i>
@@ -31,7 +56,7 @@ const TaskItems = () => {
   <i class="fa-solid fa-x text-warning me-3 pointer"></i>
   <i class="fa-solid fa-trash text-danger pointer"></i>
   </span>
-</li>
+</li> */}
 </ul>
         </>
       );
