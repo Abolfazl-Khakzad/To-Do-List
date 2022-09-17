aa20a0811b435f7b706fe0bc0fff0092e576ec96
abolfazl <a.khakzad1993@gmail.com>
2022-09-17 08:42:06 +0200

    update

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -4,62 +4,41 @@ import { taskContex } from './taskContex';
 
 const TaskItems = () => {
     const {taskItems , setTaskItems}=useContext(taskContex); 
-    // const items=useContext(taskContex);  
-    return (
-        <>
-            <ul className="list-group mt-3">
-
-                {
-                    taskItems.map(t=>(
-                        <li 
-                       
-                        className={`list-group-item d-flex justify-content-between rounded mb-2 ${ t.done ? "list-group-item-success" : "" }`}> 
-                        {t.title}
-                        <span>
-                            {
-                                t.done ? (
-                                <i class="fa-solid fa-x text-warning me-3 pointer"></i>
-                                ) : (
-                                <i class="fa-solid fa-check text-success me-3 pointer"></i>
-                                )
-                            }
-                        
-                        
-                        <i class="fa-solid fa-trash text-danger pointer"></i>
-                        </span>
-                      </li>
-                    ))
-                }
-
-{/* <li className="list-group-item d-flex justify-content-between rounded">
-  first plan
-  <span>
-  <i class="fa-solid fa-check text-success me-3 pointer"></i>
-  <i class="fa-solid fa-x text-warning me-3 pointer"></i>
-  <i class="fa-solid fa-trash text-danger pointer"></i>
-  </span>
-</li>
-
-<li className="list-group-item d-flex justify-content-between mt-2 rounded">
-  second plan
-  <span>
-  <i class="fa-solid fa-check text-success me-3 pointer"></i>
-  <i class="fa-solid fa-x text-warning me-3 pointer"></i>
-  <i class="fa-solid fa-trash text-danger pointer"></i>
-  </span>
-</li>
-
-<li className="list-group-item d-flex justify-content-between my-2 rounded">
-  third plan
-  <span>
-  <i class="fa-solid fa-check text-success me-3 pointer"></i>
-  <i class="fa-solid fa-x text-warning me-3 pointer"></i>
-  <i class="fa-solid fa-trash text-danger pointer"></i>
-  </span>
-</li> */}
-</ul>
-        </>
-      );
+    
+    if(taskItems.length){
+        return (
+            <>
+                <ul className="list-group mt-3">
+    
+                    {
+                        taskItems.map(t=>(
+                            <li 
+                           
+                            className={`list-group-item d-flex justify-content-between rounded mb-2 ${ t.done ? "list-group-item-success" : "" }`}> 
+                            {t.title}
+                            <span>
+                                {
+                                    t.done ? (
+                                    <i class="fa-solid fa-x text-warning me-3 pointer"></i>
+                                    ) : (
+                                    <i class="fa-solid fa-check text-success me-3 pointer"></i>
+                                    )
+                                }
+                            
+                            
+                            <i class="fa-solid fa-trash text-danger pointer"></i>
+                            </span>
+                          </li>
+                        ))
+                    }
+                    </ul>
+            </>
+          );
+    }else{
+        return <h4 className='text-center text-danger m-4'>there's no plan .... !</h4>
+    }
+    
 }
  
 export default TaskItems;
+
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useState } from 'react';
+import { taskContex } from './taskContex';
 
 
 const TopForm = () => {
+  const [task , setTask]=useState("");
+
+  const {taskItems , setTaskItems} = useContext(taskContex)
+
+  const handleSetTask = (event)=>{
+    setTask(event.target.value)
+  }
+
+  const handleAddTask = (event)=>{
+    event.preventDefault()
+    setTaskItems([...taskItems , {id:Math.random(), title:task , done:false}])
+  }
+
     return ( 
         <>
             <h4 className="text-center my-3 text-info">To Do List</h4>
                     <form>
                       <div className="d-flex">
-                        <input type="text" className="form-control" placeholder="next plan!!"/>
-                        <button type="submit" className="btn btn-success ms-1">Go</button>
+                        <input type="text" className="form-control" placeholder="next plan!!" value={task} onChange={handleSetTask}/>
+                        <button type="submit" className="btn btn-success ms-1" onClick={handleAddTask}>Go</button>
                       </div>
                     </form>
         </>
